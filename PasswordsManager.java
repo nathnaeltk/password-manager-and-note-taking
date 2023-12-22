1194cba94e8b2c0423b24c5528de989a49845aad
Nathnael Teketel <hellonathnael@gmail.com>
2023-12-22 10:16:43 +0300

    pass manager page version mess

--- PasswordsManager.java
+++ PasswordsManager.java
@@ -3,15 +3,14 @@ import javax.swing.*;
 
 public class PasswordsManager extends JPanel {
     private JList<String> jcomp1;
-    private JPasswordField jcomp2;
+    private JTextField jcomp2;
     private JLabel jcomp3;
     private JLabel jcomp4;
     private JTextField jcomp5;
     private JLabel jcomp6;
-    private JTextField jcomp7;
-    private JTextField jcomp8;
-    private JLabel jcomp9;
+    private JPasswordField jcomp7;
     private JLabel jcomp10;
+    private JButton jcomp11b;
     private JButton jcomp11;
     private JButton jcomp12;
     private JButton jcomp13;
@@ -23,15 +22,14 @@ public class PasswordsManager extends JPanel {
 
         //construct components
         jcomp1 = new JList<>(jcomp1Items);
-        jcomp2 = new JPasswordField(5);
+        jcomp2 = new JTextField(5);
         jcomp3 = new JLabel("Website Name");
         jcomp4 = new JLabel("Email or Username");
         jcomp5 = new JTextField(5);
         jcomp6 = new JLabel("Password");
-        jcomp7 = new JTextField(5);
-        jcomp8 = new JTextField(5);
-        jcomp9 = new JLabel("Repeat Password");
+        jcomp7 = new JPasswordField(5);
         jcomp10 = new JLabel("Save a New Password");
+        jcomp11b = new JButton("Generate Password");
         jcomp11 = new JButton("Save");
         jcomp12 = new JButton("Logout");
         jcomp13 = new JButton("Notes");
@@ -48,25 +46,23 @@ public class PasswordsManager extends JPanel {
         add(jcomp5);
         add(jcomp6);
         add(jcomp7);
-        add(jcomp8);
-        add(jcomp9);
         add(jcomp10);
+        add(jcomp11b);
         add(jcomp11);
         add(jcomp12);
         add(jcomp13);
 
         //set component bounds (only needed by Absolute Positioning)
         jcomp1.setBounds(45, 140, 160, 175);
-        jcomp2.setBounds(330, 140, 265, 35);
-        jcomp3.setBounds(235, 145, 100, 25);
-        jcomp4.setBounds(215, 185, 120, 25);
-        jcomp5.setBounds(330, 180, 265, 35);
-        jcomp6.setBounds(265, 225, 100, 25);
-        jcomp7.setBounds(330, 220, 265, 40);
-        jcomp8.setBounds(330, 265, 265, 40);
-        jcomp9.setBounds(220, 270, 105, 30);
-        jcomp10.setBounds(405, 110, 150, 25);
-        jcomp11.setBounds(495, 305, 100, 25);
+        jcomp2.setBounds(330, 160, 265, 35);
+        jcomp3.setBounds(235, 165, 100, 25);
+        jcomp4.setBounds(215, 205, 120, 25);
+        jcomp5.setBounds(330, 200, 265, 35);
+        jcomp6.setBounds(265, 245, 100, 25);
+        jcomp7.setBounds(330, 240, 265, 40);
+        jcomp10.setBounds(405, 130, 150, 25);
+        jcomp11b.setBounds(330, 285, 150, 25);
+        jcomp11.setBounds(495, 285, 100, 25);
         jcomp12.setBounds(495, 40, 100, 25);
         jcomp13.setBounds(390, 40, 100, 25);
 
