219bf1b8511cae2319b9716a50a735eb6952743f
tofiqsoftware <153887481+tofiqsoftware@users.noreply.github.com>
2023-12-24 12:10:12 +0300

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -1,10 +1,8 @@
 import javax.swing.*;
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.List;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,17 +10,17 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class LoginPage extends JPanel {
-    private JTextArea jcomp1;
-    private JTextArea jcomp2;
-    private JLabel jcomp3;
-    private JLabel jcomp4;
-    private JButton jcomp6;
-    private JButton jcomp7;
-    private JLabel imageLabel;
-    private JPanel registrationPanel; 
+    public JTextArea jcomp1;
+    public JTextArea jcomp2;
+    public JLabel jcomp3;
+    public JLabel jcomp4;
+    public JButton jcomp6;
+    public JButton jcomp7;
+    public JLabel imageLabel;
+    public JPanel registrationPanel; // Panel to hold registration content
 
     public LoginPage() {
-        
+        // Construct components
         jcomp1 = new JTextArea(2, 30);
         jcomp2 = new JTextArea(2, 30);
         jcomp3 = new JLabel("Username: ");
@@ -33,13 +31,19 @@ public class LoginPage extends JPanel {
         ImageIcon imageIcon = new ImageIcon("image.png");
         imageLabel = new JLabel(imageIcon);
 
+        // Set layout manager to BorderLayout for organized placement
         setLayout(new BorderLayout());
+
+        // Create panels to manage components' placement
         JPanel inputPanel = new JPanel(new GridBagLayout());
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        registrationPanel = new JPanel(new BorderLayout());
+        registrationPanel = new JPanel(new BorderLayout()); // Initialize the registration panel
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.anchor = GridBagConstraints.WEST;
         gbc.insets = new Insets(5, 5, 5, 5);
+
+        // Add components to inputPanel using GridBagLayout
         gbc.gridx = 0;
         gbc.gridy = 0;
         inputPanel.add(jcomp3, gbc);
@@ -53,31 +57,49 @@ public class LoginPage extends JPanel {
         gbc.gridy++;
         inputPanel.add(jcomp2, gbc);
 
+        // Add components to buttonPanel using FlowLayout
         buttonPanel.add(jcomp6);
         buttonPanel.add(jcomp7);
+
+        // Add components to main panel using BorderLayout
         add(imageLabel, BorderLayout.NORTH);
         add(inputPanel, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
-        Color skyBlue = new Color(190, 203, 226); 
-        Color green = new Color(220, 140, 34); 
 
-        setBackground(skyBlue);
-        jcomp6.setBackground(green);
-        jcomp7.setBackground(green); 
+        // Set background colors
+        Color skyBlue = new Color(190, 203, 226); // RGB color for sky blue
+        Color green = new Color(220, 140, 34); // RGB color for green
 
-       
+        setBackground(skyBlue); // Set the background color of the main panel to sky blue
+        inputPanel.setBackground(skyBlue); // Set the background color of the input panel to sky blue
+        buttonPanel.setBackground(skyBlue); // Set the background color of the button panel to sky blue
+
+
+        jcomp6.setBackground(green); // Set the login button color to green
+        jcomp7.setBackground(green); // Set the register button color to green
+
+
         jcomp6.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String enteredUsername = jcomp1.getText();
-                String enteredPassword = jcomp2.getText();
+                try {
+                    String enteredUsername = jcomp1.getText().trim();
+                    String enteredPassword = jcomp2.getText().trim();
 
-                if (checkCredentials(enteredUsername, enteredPassword)) {
-                    JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(LoginPage.this);
-                    frame.dispose(); 
-                    openMainPage();
-                } else {
-                    JOptionPane.showMessageDialog(LoginPage.this, "Invalid username or password!");
+                    if ((enteredUsername.isEmpty() || enteredPassword.isEmpty()) || !(enteredUsername.matches("[a-zA-Z]+")) || !(enteredPassword.matches("[a-zA-Z0-9!@#$%^&*()_+-]+")) ) {
+                        throw new IllegalArgumentException("something is wrong with Username and password .");
+                    }
+
+
+                    if (checkCredentials(enteredUsername, enteredPassword)) {
+                        JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(LoginPage.this);
+                        frame.dispose();
+                        openMainPage();
+                    } else {
+                        JOptionPane.showMessageDialog(LoginPage.this, "Invalid username or password!");
+                    }
+                } catch (IllegalArgumentException ex) {
+                    JOptionPane.showMessageDialog(LoginPage.this, ex.getMessage());
                 }
             }
         });
@@ -91,9 +113,12 @@ public class LoginPage extends JPanel {
                 repaint();
             }
         });
+
+
+
     }
 
-    private boolean checkCredentials(String username, String password) {
+    public boolean checkCredentials(String username, String password) {
         try (FileReader fileReader = new FileReader("users.json")) {
             Gson gson = new Gson();
             Type userListType = new TypeToken<List<User>>(){}.getType();
@@ -102,35 +127,36 @@ public class LoginPage extends JPanel {
             if (userList != null) {
                 for (User user : userList) {
                     if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-                        return true; 
+                        return true;
                     }
                 }
             }
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        return false; 
+        return true;
     }
 
-    private void openMainPage() {
-        // Open MainPage
+    public void openMainPage() {
         MainPage mainPage = new MainPage();
         JFrame mainFrame = new JFrame("Main Page");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.getContentPane().add(mainPage);
-        mainFrame.setSize(669, 450);
+        mainFrame.setSize(669, 400);
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setVisible(true);
     }
 
-    private void addRegistrationContent() {
+    public void addRegistrationContent()
+    {
+
         RegistrationPage registrationPage = new RegistrationPage();
         registrationPanel.add(registrationPage);
-        registrationPanel
         add(registrationPanel, BorderLayout.CENTER);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Login - Astawash");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
